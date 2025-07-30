b67e49e7636334c7d8040aac4bb042ab6f2e3f81
VK-1805 <2420080026@klh.edu.in>
2025-07-30 10:50:38 +0530

    Add files via upload

---
+++ TrimWhitespace.java
@@ -0,0 +1,8 @@
+package oops;
+
+class TrimWhitespace {
+    public static void main(String[] args) {
+        String s = "  Hello  ";
+        System.out.println("[" + s.trim() + "]");     
+    }
+}
