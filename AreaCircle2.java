f90c460a322573756c35eddb09880a0e9e3bed64
VK-1805 <2420080026@klh.edu.in>
2025-08-06 10:47:56 +0530

    Add files via upload

---
+++ AreaCircle2.java
@@ -0,0 +1,16 @@
+package oops; // Remove this if you don't have an 'oops' package
+
+class Circle1 {
+    void calculateArea(double radius) {
+        double area = 3.14159 * radius * radius;
+        System.out.println("Radius: " + radius);
+        System.out.println("Area of the circle: " + area);
+    }
+}
+
+public class AreaCircle2 {
+    public static void main(String[] args) {
+        Circle1 c = new Circle1();
+        c.calculateArea(2.5);
+    }
+}
